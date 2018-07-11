432acff2f8c224e1ddc38fa2c2c7048b1a5b5144
Dan Bruder <ddbruder@gmail.com>
2018-07-11 15:49:11 -0400

    Added a bunch of plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'DEB Associates',
+    siteUrl: `https://debassociates.com`,
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `6d2kjfv47hcs`,
+        accessToken: `dd274fe947669d0fb05fa8038b81b1c2db70930a6896cb7d9553d0f1eefb91f3`,
+      },
+    },
+    'gatsby-plugin-react-helmet',
+  ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+//// Implement the Gatsby API “createPages”. This is called once the
+//// data layer is bootstrapped to let plugins create pages from data.
+//exports.createPages = ({ graphql, actions }) => {
+//const { createPage } = actions
 
- // You can delete this file if you're not using it
+//return new Promise((resolve, reject) => {
+//const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+//// Query for markdown nodes to use in creating pages.
+//resolve(
+//graphql(
+//`
+//{
+//allMarkdownRemark(limit: 1000) {
+//edges {
+//node {
+//frontmatter {
+//path
+//}
+//}
+//}
+//}
+//}
+//`
+//).then(result => {
+//if (result.errors) {
+//reject(result.errors)
+//}
+
+//// Create pages for each markdown file.
+//result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//const path = node.frontmatter.path
+//createPage({
+//path,
+//component: blogPostTemplate,
+//// In your blog post template's graphql query, you can use path
+//// as a GraphQL variable to query for data from the markdown file.
+//context: {
+//path,
+//},
+//})
+//})
+//})
+//)
+//})
+//}
