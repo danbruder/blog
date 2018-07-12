c1783ea0ed5d551595b53b82b075d8989d099a0f
Dan Bruder <ddbruder@gmail.com>
2018-07-11 22:14:14 -0400

    Work on front page

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Headroom from 'react-headroom'
 
 const Header = ({ siteTitle }) => (
-  <div className="w-100 pa3 ph5-ns bg-white">
-    <div className="db dt-ns mw9 center w-100">
-      <div className="db dtc-ns v-mid tl w-50">
-        <Link to="/" className="dib f5 f4-ns fw6 mt0 mb1 link black-70">
-          {siteTitle}
-        </Link>
-      </div>
-      <div className="db dtc-ns v-mid w-100 tl tr-ns mt2 mt0-ns">
-        <Link
-          to="/specialties"
-          className="f6 fw6 hover-blue link black-70 mr2 mr3-m mr4-l dib"
-        >
-          Specialties
-        </Link>
+  <Headroom>
+    <div className="w-100 pa3 ph5-ns bg-white">
+      <div className="db dt-ns mw9 center w-100">
+        <div className="db dtc-ns v-mid tl w-50">
+          <Link to="/" className="dib f5 f4-ns fw6 mt0 mb1 link black-70">
+            {siteTitle}
+          </Link>
+        </div>
+        <div className="db dtc-ns v-mid w-100 tl tr-ns mt2 mt0-ns">
+          <Link
+            to="/specialties"
+            className="f6 fw6 hover-blue link black-70 mr2 mr3-m mr4-l dib"
+          >
+            Specialties
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
+  </Headroom>
 )
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => <Layout>test</Layout>
+const IndexPage = () => (
+  <Layout>
+    <div className="w-100 pa3 ph5-ns bg-light-gray flex-ns">
+      <div className="w-50-ns w-100">
+        <h1 className="f-subheadline-ns lh-solid">
+          We <br />Design <br />Custom <br /> Electronics
+        </h1>
+        <h3 class="f3 fw4 gray">We deliver or you don't pay.</h3>
+      </div>
+      <div className="relative h4">
+        <div
+          className="w4 h4 br3 shadow-5"
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage:
+              'url(http://www.yuppee.com/wp-content/uploads/2016/04/circuit.jpg)',
+          }}
+        />
+        <div className="mw6 h4 bg-white absolute t-0">
+          <label>hey</label>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
 
 export default IndexPage
