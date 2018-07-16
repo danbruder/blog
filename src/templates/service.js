ea6b4bc7ce8c902a33c3028d2db10c6aec4fc87a
Dan Bruder <ddbruder@gmail.com>
2018-07-16 14:11:07 -0400

    remove favicon

--- src/templates/service.js
+++ src/templates/service.js
@@ -14,22 +14,28 @@ const ServicePage = ({ data }) => (
     <div className="w-100 h5">
       <Img className="w-100 h5" sizes={get(data, 'service.image.sizes', {})} />
     </div>
-    <div className="mw9 center w-100  bg-light-gray">
-      <article className="cf ph3 ph5-ns pv5">
-        <header className="fn fl-ns w-40-ns pr4-ns">
-          <time className="f6 ttu tracked gray">Service</time>
-          <h1 className="mb3 mt0 lh-title">{get(data, 'service.title', '')}</h1>
-          <h2 className="f4 fw3 lh-copy">{get(data, 'service.summary', '')}</h2>
-        </header>
-        <div className="fn fl-ns w-60-ns">
-          <div
-            className="f5 lh-copy  mt0-ns"
-            dangerouslySetInnerHTML={{
-              __html: get(data, 'service.body.childMarkdownRemark.html', ''),
-            }}
-          />
-        </div>
-      </article>
+    <div className=" bg-light-gray">
+      <div className="mw9 center w-100  ">
+        <article className="cf ph3 ph5-ns pv5">
+          <header className="fn fl-ns w-40-ns pr4-ns">
+            <time className="f6 ttu tracked gray">Service</time>
+            <h1 className="mb3 mt0 lh-title">
+              {get(data, 'service.title', '')}
+            </h1>
+            <h2 className="f4 fw3 lh-copy">
+              {get(data, 'service.summary', '')}
+            </h2>
+          </header>
+          <div className="fn fl-ns w-60-ns">
+            <div
+              className="f5 lh-copy  mt0-ns"
+              dangerouslySetInnerHTML={{
+                __html: get(data, 'service.body.childMarkdownRemark.html', ''),
+              }}
+            />
+          </div>
+        </article>
+      </div>
     </div>
   </Layout>
 )
