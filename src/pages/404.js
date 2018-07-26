a839697b8452312ca0ab1d07a1f81a67bbedfa02
Dan Bruder <ddbruder@gmail.com>
2018-07-26 06:54:00 -0400

    404 style

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,33 +13,38 @@ const Embed = ({ id, className }) => (
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="mw7 center pa3 ph5-ns">
-      <h1>That page doesn't exist.</h1>
-      <h3>But these do:</h3>
-      <ul className="">
-        <li>
-          <Link to="/blog/reflections-on-the-book-functional-web-development-with-elixir-otp-and-phoenix">
-            Reflections on the book Functional Web Development with Elixir, OTP,
-            and Phoenix
-          </Link>
-        </li>
-        <li>
-          <Link to="/blog/about">About me</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog posts</Link>
-        </li>
-      </ul>
+    <div className="bg-gray-light">
+      <div className="mw7 center pa3 ph5-ns">
+        <h1>That page doesn't exist.</h1>
+        <h3>But these do:</h3>
+        <ul className="">
+          <li>
+            <Link to="/blog/reflections-on-the-book-functional-web-development-with-elixir-otp-and-phoenix">
+              Reflections on the book Functional Web Development with Elixir,
+              OTP, and Phoenix
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog/about">About me</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog posts</Link>
+          </li>
+        </ul>
 
-      <h3>Still here?</h3>
-      <h4>Some of my favorites:</h4>
-      <div className="flex flex-wrap justify-center">
-        <Embed className="w-100 w-50-ns" id="lDKCSheBc-8" />
-        <Embed className="w-100 w-50-ns" id="qHrN5Mf5sgo" />
-        <Embed className="w-100 w-50-ns" id="Tng6Fox8EfI" />
-        <Embed className="w-100 w-50-ns" id="eisuQefYw_o" />
-        <Embed className="w-100 w-50-ns" id="gom6nEvtl3U" />
-        <Embed className="w-100 w-50-ns" id="lO1z-7cuRYI" />
+        <br />
+        <br />
+        <br />
+        <h3>Still here?</h3>
+        <h4>Some of my favorites:</h4>
+        <div className="flex flex-wrap justify-center">
+          <Embed className="w-100 w-50-ns" id="lDKCSheBc-8" />
+          <Embed className="w-100 w-50-ns" id="qHrN5Mf5sgo" />
+          <Embed className="w-100 w-50-ns" id="Tng6Fox8EfI" />
+          <Embed className="w-100 w-50-ns" id="eisuQefYw_o" />
+          <Embed className="w-100 w-50-ns" id="gom6nEvtl3U" />
+          <Embed className="w-100 w-50-ns" id="lO1z-7cuRYI" />
+        </div>
       </div>
     </div>
   </Layout>
