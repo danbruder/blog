cbc78311d010c6edf697d9332cfdf2ac23dc8155
Dan Bruder <danbruder@hey.com>
2024-03-22 14:08:41 -0400

    fix

--- static/output.js
+++ static/output.js
@@ -1,3 +1,22 @@
-if ('paintWorklet' in CSS) {
-  CSS.paintWorklet.addModule('/paintWorklet.js');
+if ("paintWorklet" in CSS) {
+  // Safari TP fix.
+  // Safari only accepts source code as a string instead file path.
+  if (
+    navigator.userAgent.includes("Safari") &&
+    !navigator.userAgent.includes("Chrome")
+  ) {
+    (async function () {
+      const response = await fetch("/paintWorklet.js");
+      const blob = await response.blob();
+      const reader = new FileReader();
+
+      reader.addEventListener("load", () => {
+        CSS.paintWorklet.addModule(reader.result);
+      });
+
+      reader.readAsText(blob);
+    })();
+  } else {
+    CSS.paintWorklet.addModule("/paintWorklet.js");
+  }
 }
